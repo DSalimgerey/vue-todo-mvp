870f191623c177693676236fc9da9d527d67cbb0
DidarSalimgerey <didarsalimgerey.dev@gmail.com>
2023-03-16 20:24:04 +0600

    add datepicker component and modelValue prop validation

---
+++ src/components/datepicker/v-datepicker.vue
@@ -0,0 +1,19 @@
+<script>
+import { isArray } from '../../utils'
+import { isDate, isValidRange } from './utils'
+
+export default {
+  name: 'v-datepicker',
+  props: {
+    modelValue: {
+      type: [Date, Array],
+      required: true,
+      validator: (value) => (isArray(value) ? isValidRange(value) : isDate(value))
+    }
+  }
+}
+</script>
+
+<template>
+  <div></div>
+</template>
