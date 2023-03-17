d1c0bf83fb2117b780f79620cb7a9b86141f9532
DidarSalimgerey <didarsalimgerey.dev@gmail.com>
2023-03-17 19:19:05 +0600

    add 'focus' and 'isActiveElement' utility unctions

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,7 @@
 import { unref } from 'vue'
 
 export const isArray = (value) => Array.isArray(value)
+
 export const focus = (el) => unref(el)?.focus()
+
+export const isActiveElement = (el) => document.activeElement === unref(el)
