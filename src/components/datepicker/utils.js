05506ad3f123933f31710607b2e8f0a2d78f6743
DidarSalimgerey <didarsalimgerey.dev@gmail.com>
2023-03-17 20:17:06 +0600

    add 'isBetween' and 'isAfter' functions

--- src/components/datepicker/utils.js
+++ src/components/datepicker/utils.js
@@ -35,3 +35,21 @@ export const format = (date, template) => dayjs(date).format(template)
 
 export const isBetween = (date, start, end, unit = 'date') =>
   dayjs(date).isBetween(dayjs(start).subtract(1, 'day'), dayjs(end).add(1, 'day'), unit)
+
+/**
+ * this indicates whether the Day.js object is before the other supplied date-time.
+ * @param {dayjs.ConfigType} d1
+ * @param {dayjs.ConfigType} d2
+ * @param {dayjs.OpUnitType} unit
+ * @returns {boolean}
+ */
+export const isBefore = (d1, d2, unit = 'date') => dayjs(d1).isBefore(d2, unit)
+
+/**
+ * This indicates whether the Day.js object is after the other supplied date-time.
+ * @param {dayjs.ConfigType} d1
+ * @param {dayjs.ConfigType} d2
+ * @param {dayjs.OpUnitType} unit
+ * @returns {boolean}
+ */
+export const isAfter = (d1, d2, unit = 'date') => dayjs(d1).isAfter(d2, unit)
