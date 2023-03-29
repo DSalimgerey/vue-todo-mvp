7cefe578a9f90dd8394f8a89291e594c812a66c6
DidarSalimgerey <didarsalimgerey.dev@gmail.com>
2023-03-29 19:07:46 +0600

    add key codes constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,8 @@
 export const DAYS_AMOUNT_OF_CALENDAR = 42
 export const WEEKDAYS = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su']
 export const BASE_DATE_FORMAT = 'M-D-YYYY'
+export const ARROW_UP = 'ArrowUp'
+export const ARROW_RIGHT = 'ArrowRight'
+export const ARROW_DOWN = 'ArrowDown'
+export const ARROW_LEFT = 'ArrowLeft'
+export const ENTER = 'Enter'
