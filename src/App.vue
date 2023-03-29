a4b48f530c64589fd4df7e48026f22f2934e8d76
DidarSalimgerey <didarsalimgerey.dev@gmail.com>
2023-03-29 17:59:50 +0600

    update 'dueDate' property value of specified todo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <script setup>
+import { ref, watch } from 'vue'
 import VTodoItem from './components/v-todo-item.vue'
+
+const todos = ref([
+  {
+    id: '1213',
+    title: 'test',
+    dueDate: '4-12-2023'
+  }
+])
+
+const updateDueDate = ({ value, id }) => {
+  const idx = todos.value.findIndex((t) => t.id === id)
+  const todo = todos.value[idx]
+  const updatedTodo = Object.assign(todo, { dueDate: value })
+  todos.value.splice(idx, 1, updatedTodo)
+}
+
+watch(
+  () => todos.value,
+  (value) => console.log(value),
+  { deep: true }
+)
 </script>
 
 <template>
@@ -11,7 +33,12 @@ import VTodoItem from './components/v-todo-item.vue'
         </div>
 
         <ul>
-          <v-todo-item v-for="todo in 2" :key="todo"></v-todo-item>
+          <v-todo-item
+            v-for="todo in todos"
+            :key="todo"
+            :todo="todo"
+            @update-date="updateDueDate($event)"
+          ></v-todo-item>
         </ul>
       </div>
     </div>
