5fd66bc997762724ed536166d848f50abbe4f3a5
DidarSalimgerey <didarsalimgerey.dev@gmail.com>
2023-03-30 19:17:38 +0600

    add new date and range formats with time

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,17 @@
 export const DAYS_AMOUNT_OF_CALENDAR = 42
 export const WEEKDAYS = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su']
 export const BASE_DATE_FORMAT = 'M-D-YYYY'
+export const BASE_DATE_FORMAT_WITH_TIME = 'M-D-YYYY HH:mm'
+export const DISPLAY_DATE_FORMAT = 'MMM D, YYYY'
+export const DISPLAY_DATE_FORMAT_WITH_TIME = 'MMM D, YYYY HH:mm'
 export const ARROW_UP = 'ArrowUp'
 export const ARROW_RIGHT = 'ArrowRight'
 export const ARROW_DOWN = 'ArrowDown'
 export const ARROW_LEFT = 'ArrowLeft'
 export const ENTER = 'Enter'
+export const calendarMode = Object.freeze({
+  DATE: 'date',
+  DATE_TIME: 'date-time',
+  RANGE: 'range',
+  RANGE_TIME: 'range-time'
+})
