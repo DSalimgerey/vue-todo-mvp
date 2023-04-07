9311d376d3ca78c0c4e59451dde862622b8e55fa
DidarSalimgerey <didarsalimgerey.dev@gmail.com>
2023-04-07 14:07:50 +0600

    highlight todo item priority differences

--- src/components/v-todo-item.vue
+++ src/components/v-todo-item.vue
@@ -16,6 +16,28 @@ export default {
     }
   },
 
+  computed: {
+    priority() {
+      let value
+      switch (this.todo.priority) {
+        case 1:
+          value = 'border-red-500 bg-red-50 border-[2px]'
+          break
+        case 2:
+          value = 'border-orange-500 bg-orange-50 border-[2px]'
+          break
+        case 3:
+          value = 'border-blue-500 bg-blue-50 border-[2px]'
+          break
+        default:
+          value = 'border-gray-400'
+          break
+      }
+
+      return value
+    }
+  },
+
   watch: {
     date(value) {
       this.$emit('updateDate', { value, id: this.todo.id })
@@ -28,6 +50,7 @@ export default {
   <li class="flex py-[3px]">
     <button
       class="w-[18px] h-[18px] border border-gray-400 rounded-full mr-[10px] mt-[2px]"
+      :class="priority"
     ></button>
     <div>
       <div>
